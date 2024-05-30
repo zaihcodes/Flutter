1f050c5b8e1014555ebfed3c05361d8a7bb34cc0
ZI-DreamWork <zaihwork@gmail.com>
2024-05-30 22:23:08 +0100

    Created the first version of home screen

--- portfolio_app/lib/main.dart
+++ portfolio_app/lib/main.dart
@@ -14,11 +14,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> categories = [
+    'All',
+    'Illustration',
+    'UIUX Design',
+    'Graphic Design',
+    'Icons'
+  ];
+
+  int activeTab = 0;
+  bool run = true;
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -28,8 +44,7 @@ class HomeScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(50),
-                  child: Image.network(
-                      'https://randomuser.me/api/portraits/men/4.jpg'),
+                  child: Image.asset('assets/images/green_ai_0.jpg'),
                 )),
           )
         ],
@@ -52,8 +67,103 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Today\'s popular',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          'Designs',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: Image.asset(
+                        "assets/images/green_ai_1.jpg",
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 40,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  separatorBuilder: (context, index) => const SizedBox(
+                    width: 20,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          activeTab = index;
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: index == activeTab
+                              ? Colors.green.shade200
+                              : Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(categories[index]),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              postCart('assets/images/green_ai_2.jpg'),
+              postCart('assets/images/green_ai_3.jpg'),
+              postCart('assets/images/green_ai_4.jpg'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget postCart(String image) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(image),
       ),
     );
   }
