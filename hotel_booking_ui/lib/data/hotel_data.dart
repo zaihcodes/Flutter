ea0b49eb0a3a1701f5fe107dfa9cdc5d0ab79962
zaihcodes <zaihwork@gmail.com>
2024-06-27 11:10:51 +0100

    add hotel model and some data

---
+++ hotel_booking_ui/lib/data/hotel_data.dart
@@ -0,0 +1,68 @@
+import 'package:hotel_booking_ui/model/hotel_model.dart';
+
+class HotelData {
+  static List<HotelModel> hotelList = <HotelModel>[
+    HotelModel(
+      imagePath: 'assets/hotel/hotel_1.png',
+      titleTxt: 'Grand Royal Hotel',
+      subTxt: 'Wembley, London',
+      dist: 2.0,
+      reviews: 80,
+      rating: 4.4,
+      perNight: 180,
+    ),
+    HotelModel(
+      imagePath: 'assets/hotel/hotel_2.png',
+      titleTxt: 'Queen Hotel',
+      subTxt: 'Wembley, London',
+      dist: 4.0,
+      reviews: 74,
+      rating: 4.5,
+      perNight: 200,
+    ),
+    HotelModel(
+      imagePath: 'assets/hotel/hotel_3.png',
+      titleTxt: 'Grand Royal Hotel',
+      subTxt: 'Wembley, London',
+      dist: 3.0,
+      reviews: 62,
+      rating: 4.0,
+      perNight: 60,
+    ),
+    HotelModel(
+      imagePath: 'assets/hotel/hotel_4.png',
+      titleTxt: 'Queen Hotel',
+      subTxt: 'Wembley, London',
+      dist: 7.0,
+      reviews: 90,
+      rating: 4.4,
+      perNight: 170,
+    ),
+    HotelModel(
+      imagePath: 'assets/hotel/hotel_5.png',
+      titleTxt: 'Grand Royal Hotel',
+      subTxt: 'Wembley, London',
+      dist: 2.0,
+      reviews: 240,
+      rating: 4.5,
+      perNight: 200,
+    ),
+  ];
+
+  static List<String> popularFList = <String>[
+    'Free Breakfast',
+    'Free Parking',
+    'Pool',
+    'Pet Friendly',
+    'Free wifi',
+  ];
+
+  static List<String> accomodationList = [
+    'All',
+    'Apartment',
+    'Home',
+    'Villa',
+    'Hotel',
+    'Resort',
+  ];
+}
